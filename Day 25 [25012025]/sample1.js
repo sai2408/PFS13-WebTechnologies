e4bc067a9bbd317be38a7a07c6ab3b605a57eb69
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-25 15:41:45 +0530

    Add files via upload

---
+++ Day 25 [25012025]/sample1.js
@@ -0,0 +1,65 @@
+///Data Types
+//Primitive
+var a = 10;
+console.log(a);
+console.log(typeof(a));
+var a2 = 12345678909876543212345;
+console.log(a2)
+console.log(typeof(a2));
+var b1 = 3.4567;
+console.log(b1)
+console.log(typeof(b1))
+var c1 = "hello world";
+console.log(c1);
+console.log(typeof(c1));
+var d1 = false;
+console.log(d1);
+console.log(typeof(d1));
+var d1 = true;
+console.log(d1);
+console.log(typeof(d1));
+var e;
+console.log(e);
+console.log(typeof(e));
+var f = null;
+console.log(f);
+console.log(typeof(f));
+var x1 = Symbol("sai vardhan");
+console.log(x1);
+console.log(typeof(x1));
+var x1 = Symbol(12345);
+console.log(x1);
+console.log(typeof(x1));
+//Reference
+//object
+var k1 = {1:10,"a":"abcd",1:"yxz","anc":123};
+console.log(k1);
+console.log(typeof(k1));
+//function
+function abc(){
+    console.log("I am a function");
+}
+abc();
+//array
+var k2 = [1,2,3,5,4,5,"asdS"];
+console.log(k2);
+console.log(typeof(k2));
+//Special
+//Big int
+var a = BigInt(1234567898765432234567890);
+console.log(a);
+console.log(typeof(a));
+//NaN
+var y1 = NaN;
+console.log(y1);
+console.log(typeof(y1));
+var y2 = Number.NaN;
+console.log(y2);
+console.log(typeof(y2));
+//Infinity
+var i1 = Infinity;
+console.log(i1);
+console.log(typeof(i1));
+var i1 = -Infinity;
+console.log(i1);
+console.log(typeof(i1));
---
+++ Day 25 [25012025]/sample2.js
@@ -0,0 +1,36 @@
+//Operators in Js
+//Arithemetic Opr
+/*
+var a = 10;
+var b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+console.log(23%2);
+console.log(a/b);
+console.log(23/2);
+console.log(4**3);
+*/
+//Assignment Operators
+var a1 = 10;
+a1 = a1 + 2;
+console.log(a1);
+var a2 = 20;
+a2 += 2;
+console.log(a2);
+var b1 = 10;
+b1 -= 2;
+console.log(b1);
+var c1 = 10;
+c1 *= 3;
+console.log(c1);
+var d1 = 12;
+d1 /= 2;
+console.log(d1);
+var e1 = 13;
+e1 %= 2;
+console.log(e1);
+var f1 = 2;
+f1 **= 4;
+console.log(f1);
