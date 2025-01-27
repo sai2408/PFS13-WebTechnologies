732c32128b5273d4cf3b006871a2ecf64f8feb64
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-27 15:49:21 +0530

    Add files via upload

---
+++ Day 26 [27012025]/sampel1.js
@@ -0,0 +1,60 @@
+//Comparision Operators
+/*
+console.log("3" == 3);
+console.log("3" == "3");
+console.log("3" == "4");
+console.log(4 == 3);
+console.log("3" === 3);
+console.log("3" === "3");
+console.log("3" != 4);
+console.log(10>3);
+console.log(10>=10);
+console.log(10<10);
+console.log(10<=10);
+console.log(3 != "3");
+console.log(3 !== "3");
+*/
+//Logical Operators
+/*
+console.log((10>1) && (10>1));
+console.log((10>1) && (10>10));
+console.log((10>10) && (10>1));
+console.log((10>10) || (10>10));
+console.log((10>1) || (10>10));
+console.log((10>10) || (10>1));
+console.log(!(10>1));
+*/
+//Unary Operators
+/*
+var x = "10";
+console.log(-x);
+console.log(+x);
+*/
+//Ternary Operator
+/*
+var x = (10 > 10) ? "Hello" : "World";
+console.log(x);
+var x = (10 >= 10) ? "Hello" : "World";
+console.log(x);
+*/
+//Inc & Dec Operator
+/*
+var a = 10;
+console.log(a++);
+console.log(a);
+var b = 10
+console.log(++b)
+console.log(b);
+var x = 10;
+console.log(x--);
+console.log(x);
+var y = 10;
+console.log(--y);
+console.log(y);
+*/
+//Str concat Operator
+/*
+var a = "Hello";
+var b = "World";
+console.log(a+" "+b);
+*/
