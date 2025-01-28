2fd75592fbb60cb89cf4fda98689bc8a962193c0
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-28 15:48:16 +0530

    Add files via upload

---
+++ Day 27 [28012025]/sample.js
@@ -0,0 +1,68 @@
+//IF
+/* 
+var x = 11;
+if (x%2==0){
+    console.log("Even Number");
+}
+*/
+//IF ELSE
+/*
+var x = 11;
+if (x%2==0){
+    console.log("Even Number");
+}
+else{
+    console.log("Odd number");
+}
+*/
+//IF ELSE IF ELSE
+/*
+var x = 101;
+if (x==0){
+    console.log("Number is Zero");
+}
+else if (x%2==0){
+    console.log("Even Number");
+}
+else{
+    console.log("Odd Number");
+}
+*/
+//SWITCH CASE
+/*
+var x = 14;
+switch (x){
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    default:
+        console.log("Number is invalid");
+}
+*/
+// NESTED CONDITIONAL STATEMENTS
+/*
+var x = 1;
+if (x>0){
+    if (x%2==0){
+        console.log("Positive even Number");
+    }
+    else{
+        console.log("Positive Odd Number");
+    }
+}
+else{
+    if (x%2==0){
+        console.log("Negetive Even Number");
+    }
+    else{
+        console.log("Neegtive Odd Number");
+    }
+}
+*/
+
